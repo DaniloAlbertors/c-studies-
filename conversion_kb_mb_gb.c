a4dbaa50c93026e99a6474a1aff4f1474457775a
Danilo Alberto <danilossalbertoss@gmail.com>
2026-08-26 21:01:08 -0300

    Update conversion_kb_mb_gb.c

--- conversion_kb_mb_gb.c
+++ conversion_kb_mb_gb.c
@@ -2,17 +2,16 @@
 
 float k, m, g;
 
-int main(){
+int main() {
 
-    printf("Digite o valor dos kilobyte's: ");
+    printf("Enter the value in kilobytes: ");
     scanf("%f", &k);
 
-    m = (float) k / 1000;
+    m = (float)k / 1000;
 
-    g = (float) m / 1000;
+    g = (float)m / 1000;
 
-    printf("A quantidade de kilobyte's em mega: %.2f e em giga: %.2f", m, g);
+    printf("The amount in megabytes: %.2f and in gigabytes: %.2f", m, g);
+
     return 0;
-
-
 }
