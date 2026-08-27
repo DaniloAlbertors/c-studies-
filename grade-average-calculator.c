cb9ba804b3f566ebc3a66acf1a70644ae62b76cc
Danilo Alberto <danilossalbertoss@gmail.com>
2026-08-26 21:12:56 -0300

    Update and rename exercicio11.c to grade-average-calculator.c

---
+++ grade-average-calculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+float g1, g2, g3, g4, average;
+
+int main(){
+
+    printf("First evaluation grade: ");
+    scanf("%f", &g1);
+
+    printf("Second evaluation grade: ");
+    scanf("%f", &g2);
+
+    printf("Third evaluation grade: ");
+    scanf("%f", &g3);
+
+    printf("Fourth evaluation grade: ");
+    scanf("%f", &g4);
+
+    average = ((float)(g1 + g2 + g3 + g4)/ 4);
+
+    printf("The average grade is: %.2f", average);
+    return 0;
+
+}
