7a38fcc2c366e3fe8fa90f228791929d87a6c77a
Danilo Alberto <danilossalbertoss@gmail.com>
2026-09-01 14:46:54 -0300

    Create leap_year_checker.c

---
+++ leap_year_checker.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int year;
+
+int main() { 
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+        printf("The year is a leap year!");
+    }
+    else {
+        printf("The year is not a leap year!");
+    }
+    return 0;
+}
